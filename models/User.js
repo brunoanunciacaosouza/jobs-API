9b14b0bd996137c3fd611972eef3a037f94f9cf8
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-03 16:50:20 -0300

    compare password

--- models/User.js
+++ models/User.js
@@ -23,7 +23,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide password'],
     minlength: 6,
-    select: false,
   },
 });
 
@@ -34,6 +33,10 @@ UserSchema.pre('save', async function () {
 
 UserSchema.methods.CreateJWT = function () {
   return jwt.sign({ userId: this._id, name: this.name }, process.env.JTW_SECRET, { expiresIn: process.env.JWT_LIFETIME })
-}
+};
+
+UserSchema.methods.comparePassword = async function (candidate) {
+  return await bcrypt.compare(candidate, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
