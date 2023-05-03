30c7ae5e6be7dfe05ed0002109241d870d2e9888
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-03 12:14:22 -0300

    routes

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('<h1>Jobs Api</h1>')
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
