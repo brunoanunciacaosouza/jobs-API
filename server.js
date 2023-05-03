f3334dfed63cbcca3ee89094031bdc8fb8a6c102
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-03 12:24:50 -0300

    connect database

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const app = require('./app');
+const connectDB = require('./db/connect');
 
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
     });
