c67b686498f0d26eff9012611d3787c31e5b42c1
Bruno Souza <brunoanunciacaosouza@gmail.com>
2023-05-04 21:50:30 -0300

    custom error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,3 @@
-const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
@@ -6,10 +5,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong try again later',
   };
-
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
 
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value enter for ${Object.keys(err.keyValue)} field, please choose another value`;
